d0562c3573530fe69776eb2434a12406147f816e
Mark Abrams <mabrams@helpguest.com>
2020-05-22 15:26:00 -0400

    remove erroneous global reference

--- ui/startup.py
+++ ui/startup.py
@@ -103,7 +103,6 @@ def runapp():
     app.run(host="0.0.0.0", port=80, threaded=True)
 
 if __name__ == "__main__":
-    global piid
     piid = getUniqueId()
     # fire up the input form
     runapp()
